639512341ae3df3c0ca175ef55eabdd85ecf5d6a
Sarita <saritanaik13@gmail.com>
2019-09-07 02:40:43 +0530

    Final Changes made

--- execute.py
+++ execute.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from new import Multi_Variable
+from new import Uni_Variable
 
 titanic = pd.read_csv("D:\\Sarita Naik\\Visual Studio Codes\\Statistical Analysis\\ExploratoryAnalysis\\train.csv")
 
 bs = Multi_Variable()
-a = bs.group_func(titanic,['Sex'],['Age'],'count')
-b = bs.multi_func(titanic,'Sex')
-c = bs.graph_func(titanic,'Survived','PassengerId')
-print(c)
+vs = Uni_Variable()
+#a = bs.group_func(titanic,'Sex','Fare','median')
+#b = bs.multi_func(titanic,'Sex')
+#bs.graph_func(titanic,'Sex','Embarked')
+a = vs.multi_func(titanic,'Age', 'mean')
+print(a)
--- new.py
+++ new.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class Multi_Variable:
     
     def __init__(self):
@@ -12,12 +11,45 @@ class Multi_Variable:
         value = df_1.pivot_table(index = value_1, values = value_2, aggfunc = func_1)
         return value
     
-    def multi_func(self,df_1,value_1):
-        value = df_1[value_1].value_counts()
-        return value
-
     def graph_func(self,df_1,value_1,value_2):
         type_1 = df_1[value_1].dtype
         type_2 = df_1[value_2].dtype
 
-        if(type_1 == 'int64' and type_2 == 'int64'):
+        if((type_1 == 'int64'or type_1 == 'float64') and (type_2 == 'int64' or type_2 == 'float64')):
+            plt.scatter(df_1[value_1],df_1[value_2])
+            plt.show()
+        elif((type_1 != 'int64'and type_1 != 'float64') and (type_2 != 'int64' and type_2 != 'float64')):
+            a = df_1.pivot_table(index = value_2, values = value_1, aggfunc = 'count')
+            print(a)
+        else:
+            plt.figure(figsize=(7,7))
+            plt.title("Categorical and Quantitative Graphs")
+
+            if(type_1 == 'int64' or type_1 == 'float64'):
+                plt.bar(df_1[value_2],df_1[value_1])
+                plt.xlabel(value_2)
+                plt.ylabel(value_1)    
+                plt.show()
+            else:
+                plt.bar(df_1[value_1],df_1[value_2])
+                plt.xlabel(value_1)
+                plt.ylabel(value_2)    
+                plt.show()
+
+class Uni_Variable:
+    def __init__(self):
+        pass
+
+    def multi_func(self,df_1,value_1,func_1):
+        if(func_1 == 'count'):
+            return df_1[value_1].value_counts()
+        elif(func_1 == 'mean'):
+            return df_1[value_1].mean()
+        elif(func_1 == 'median'):
+            return df_1[value_1].median()
+        else:
+            return "Mention a valid function among count, mean, median"
+        
+    def bs_boxplot(self, bs_value):
+        plt.boxplot(bs_value)
+        plt.show()
