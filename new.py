91bcc5a86d5174c36880f2c137b368af857ac98e
Sarita <saritanaik13@gmail.com>
2019-09-06 22:31:03 +0530

    commit new changes

--- new.py
+++ new.py
@@ -8,7 +8,16 @@ class Multi_Variable:
     def __init__(self):
         pass
 
-    def multi_func(self,val_1,val_2,type_1,type_2):
-        # Calculate center and spread values
-        
-        
+    def group_func(self,df_1,value_1,value_2,func_1):
+        value = df_1.pivot_table(index = value_1, values = value_2, aggfunc = func_1)
+        return value
+    
+    def multi_func(self,df_1,value_1):
+        value = df_1[value_1].value_counts()
+        return value
+
+    def graph_func(self,df_1,value_1,value_2):
+        type_1 = df_1[value_1].dtype
+        type_2 = df_1[value_2].dtype
+
+        if(type_1 == 'int64' and type_2 == 'int64'):
